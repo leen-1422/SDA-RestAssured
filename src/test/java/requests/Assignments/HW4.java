c68e860a7e900f21f8cd37c312e73d9b784367c6
leen-1422 <leen8288@yahoo.com>
2024-04-17 00:38:31 -1200

    assignment-4

--- src/test/java/requests/Assignments/HW4.java
+++ src/test/java/requests/Assignments/HW4.java
@@ -1,4 +1,53 @@
 package requests.Assignments;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.*;
+
 public class HW4 {
+     /*
+       Given
+           https://reqres.in/api/users/2
+       When
+           User send GET Request to the URL
+       Then
+           HTTP Status Code should be 200
+       And
+           Response format should be "application/json"
+       And
+           "email" is "janet.weaver@reqres.in",
+       And
+           "first_name" is "Janet"
+       And
+           "last_name" is "Weaver"
+       And
+           "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
+    */
+
+    @Test
+    public void AssertHeadersInfo() {
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        response.prettyPrint();
+
+        // Aseertions
+        response.then().statusCode(200);
+
+        response.then().contentType("application/json");
+
+        response
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data.email", equalTo("janet.weaver@reqres.in"),
+                        "data.first_name", equalTo("Janet"),
+                        "data.last_name", equalTo("Weaver"),
+                        "support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!")
+                        );
+    }
+
+
 }
+

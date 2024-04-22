660a1a454149790b4962a9535a18e06a86e4e043
leen-1422 <leen8288@yahoo.com>
2024-04-22 10:54:47 -1200

    assignment-11

--- src/test/java/requests/Assignments/HW11.java
+++ src/test/java/requests/Assignments/HW11.java
@@ -1,5 +1,12 @@
 package requests.Assignments;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class HW11 {
     /*
     Given
@@ -12,4 +19,20 @@ public class HW11 {
     Note: Print using JsonPath: response.jsonPath().prettyPrint();
 
 */
+
+    @Test
+    public void numberOfWomen() {
+
+        Response response = RestAssured.get("https://automationexercise.com/api/productsList");
+
+        response.jsonPath().prettyPrint();
+
+        JsonPath jsonPath = response.jsonPath();
+
+
+        int numberOfWomen = jsonPath.getList("products.findAll{it.category.usertype.usertype=='Women'}").size();
+        System.out.println("numberOfWomen = " + numberOfWomen);
+
+        assertEquals(numberOfWomen, 12);
+    }
 }

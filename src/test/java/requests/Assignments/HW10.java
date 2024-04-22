a144c27894b4e38d91669385f641d813d3da0189
leen-1422 <leen8288@yahoo.com>
2024-04-22 10:46:04 -1200

    assignment-10

--- src/test/java/requests/Assignments/HW10.java
+++ src/test/java/requests/Assignments/HW10.java
@@ -1,7 +1,30 @@
 package requests.Assignments;
 
-public class HW10 {
+import base_urls.CreateUserBaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+public class HW10 extends CreateUserBaseUrl {
     //Using the https://petstore.swagger.io/ document,
     // write an automation test that finds the number of "pets" with the status "available"
+    // https://petstore.swagger.io/v2/pet/findByStatus?status=available
     // and asserts that there are more than 100.
+
+    @Test
+    public void petWithAvailableStatus(){
+        //Set the Url
+        spec.pathParams("first", "v2", "second","pet", "third", "findByStatus").queryParams("status", "available");
+
+        Response response = given(spec).get("{first}/{second}/{third}");
+
+        response.prettyPrint();
+
+        // assert
+        response.then().body("", hasSize(greaterThan(100)));
+
+    }
 }
